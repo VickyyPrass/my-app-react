53b1c1ad441f233877c4392436c30f1de26972f6
VickyyPrass <vickygalih31@gmail.com>
2023-03-16 20:55:50 +0700

    update file app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Vicky disini,hehehe</p>
+                <p>hello world</p>
             </header>
         </div>
     );
